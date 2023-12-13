17bafd139811263992c474f62b5893383a546fc0
Vaughn Clayton <vaughn.clayton@servicetrade.com>
2023-12-13 12:03:56 -0500

    Day 10 WIP

--- day10.ex
+++ day10.ex
@@ -13,15 +13,43 @@ defmodule Aoc do
 		|> Enum.map(fn row -> String.graphemes(row) |> List.to_tuple end)
 		|> List.to_tuple
 		|> IO.inspect
-		%{size: tuple_size(grid), grid: grid}
+		[start] = find_start(grid)
+		dirs = find_dir(grid, start)
+		%{size: tuple_size(grid), start: start, grid: grid}
 	end
 
-	def parse_loops(inputs) do
-		# for y <- inputs, x <-
+	def find_start(grid) do
+		for x <- 0..tuple_size(grid)-1, y <- 0..tuple_size(grid)-1, getxy(grid, x, y) == "S", do: {x,y}
+	end
+
+	def connects?(grid, {x,y}, :right) do
+		case getxy(grid, x+1, y) do
+			"-" -> true
+			"7" -> true
+			"J" -> true
+			_ -> false
+		end
+	end
+	def find_dir(grid, {x,y}) do
+		[:right] |> Enum.filter(fn dir -> connects?(grid, {x, y}, dir) end)
+	end
+
+	def getxy(grid, x, y) when x < 0 or y < 0, do: nil
+	def getxy(grid, x, y) when x >= tuple_size(grid) or y >= tuple_size(grid), do: nil
+	def getxy(grid, x, y), do: elem(elem(grid, y), x)
+	def parse_loops(%{size: s, grid: g, start: start}) do
+		for x <- 0..s-1, y <- 0..s-1, getxy(g, x, y) == "S", do: {x,y}
+	end
+	def follow_loop(start, {x,y}, grid, path) when start == {x,y}, do: path
+	def follow_loop(start, {x,y}, grid, path) do
+		next = case getxy(grid, x, y) do
+			"-" -> :foo
+		end
+		[next | follow_loop(start, next, grid, path)]
 	end
 
 	def part1(inputs) do
-		inputs
+		parse_loops(inputs)
 	end
 
 	def part2(inputs) do
